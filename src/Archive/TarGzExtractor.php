394ee53b88e05d063aa0dbbcbb58b12faa503ced
Laurent Clouet <laurent.clouet@westwing.pl>
2020-11-02 22:17:39 +0100

    phpcs fix

--- src/Archive/TarGzExtractor.php
+++ src/Archive/TarGzExtractor.php
@@ -13,6 +13,9 @@ use const DIRECTORY_SEPARATOR;
 
 final class TarGzExtractor implements Extractor
 {
+    /**
+     * @inheritDoc
+     */
     public function extract(string $archive, string $destination): array
     {
         $tarGzData = new PharData($archive);
