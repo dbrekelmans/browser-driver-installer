7a5191d0cde95ea3a090bdb7daac1b3409fa1856
Laurent Clouet <laurent.clouet@westwing.pl>
2020-11-05 22:22:30 +0100

    phpcs fix

--- src/Driver/GeckoDriver/VersionResolver.php
+++ src/Driver/GeckoDriver/VersionResolver.php
@@ -11,8 +11,8 @@ use DBrekelmans\BrowserDriverInstaller\Version;
 use RuntimeException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
-use function Safe\krsort;
 use function Safe\json_decode;
+use function Safe\krsort;
 use function Safe\sprintf;
 
 final class VersionResolver implements VersionResolverInterface
