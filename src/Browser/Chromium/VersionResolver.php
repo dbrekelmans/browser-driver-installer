9849d8a1f65414abdbde316bcedaf97cbc0ce9cc
Laurent Clouet <laurent.clouet@westwing.pl>
2020-11-15 15:46:26 +0100

    Get correct Chrome Version on windows (#41)

--- src/Browser/Chromium/VersionResolver.php
+++ src/Browser/Chromium/VersionResolver.php
@@ -10,12 +10,21 @@ use DBrekelmans\BrowserDriverInstaller\CommandLine\CommandLineEnvironment;
 use DBrekelmans\BrowserDriverInstaller\Exception\NotImplemented;
 use DBrekelmans\BrowserDriverInstaller\OperatingSystem\OperatingSystem;
 use DBrekelmans\BrowserDriverInstaller\Version;
+use InvalidArgumentException;
 use RuntimeException;
+use Safe\Exceptions\StringsException;
 
 use function Safe\sprintf;
 
 final class VersionResolver implements VersionResolverInterface
 {
+    private const REG_KEY_STABLE = '8A69D345-D564-463c-AFF1-A69D9E530F96';
+    private const REG_KEY_BETA = '8237E44A-0054-442C-B6B6-EA0509993955';
+    private const REG_KEY_DEV = '401C381F-E0DE-4B85-8BD8-3F3F14FBDA57';
+    private const REG_KEY_CANARY = '4ea16ac7-fd5a-47c3-875b-dbf4a2008c20';
+    private const VERSION_REG_QUERY_LOCAL_MACHINE = 'reg query HKLM\Software\Google\Update\Clients\{%s} /v pv /reg:32 2> NUL';
+    private const VERSION_REG_QUERY_CURRENT_USER = 'reg query HKCU\Software\Google\Update\Clients\{%s} /v pv /reg:32 2> NUL';
+
     /** @var CommandLineEnvironment */
     private $commandLineEnvironment;
 
@@ -37,9 +46,15 @@ final class VersionResolver implements VersionResolverInterface
         }
 
         if ($operatingSystem->equals(OperatingSystem::WINDOWS())) {
-            return $this->getVersionFromCommandLine(
-                sprintf('wmic datafile where name="%s" get Version /value', $path)
-            );
+            foreach (self::getWindowsCommandsForVersion() as $possibleCommand) {
+                try {
+                    return $this->getVersionFromCommandLine($possibleCommand);
+                } catch (InvalidArgumentException $exception) {
+                    // @ignoreException
+                }
+            }
+
+            throw new InvalidArgumentException('Version could not be determined.');
         }
 
         throw NotImplemented::feature(
@@ -68,5 +83,29 @@ final class VersionResolver implements VersionResolverInterface
                 $exception
             );
         }
+    }
+
+    /**
+     * Provide potential commands to determine Chromium Version on Windows
+     *
+     * @see https://bugs.chromium.org/p/chromium/issues/attachmentText?aid=387709
+     * @see https://bugs.chromium.org/p/chromium/issues/detail?id=158372
+     *
+     * @return string[]
+     *
+     * @throws StringsException
+     */
+    private static function getWindowsCommandsForVersion(): array
+    {
+        $commands = [];
+        foreach ([self::REG_KEY_STABLE, self::REG_KEY_BETA, self::REG_KEY_DEV] as $regKey) {
+            $commands[] = sprintf(self::VERSION_REG_QUERY_LOCAL_MACHINE, $regKey);
+        }
+
+        foreach ([self::REG_KEY_STABLE, self::REG_KEY_BETA, self::REG_KEY_DEV, self::REG_KEY_CANARY] as $regKey) {
+            $commands[] = sprintf(self::VERSION_REG_QUERY_CURRENT_USER, $regKey);
+        }
+
+        return $commands;
     }
 }
--- src/Browser/GoogleChrome/VersionResolver.php
+++ src/Browser/GoogleChrome/VersionResolver.php
@@ -10,12 +10,20 @@ use DBrekelmans\BrowserDriverInstaller\CommandLine\CommandLineEnvironment;
 use DBrekelmans\BrowserDriverInstaller\Exception\NotImplemented;
 use DBrekelmans\BrowserDriverInstaller\OperatingSystem\OperatingSystem;
 use DBrekelmans\BrowserDriverInstaller\Version;
-use RuntimeException;
+use InvalidArgumentException;
+use Safe\Exceptions\StringsException;
 
 use function Safe\sprintf;
 
 final class VersionResolver implements VersionResolverInterface
 {
+    private const REG_KEY_STABLE = '8A69D345-D564-463c-AFF1-A69D9E530F96';
+    private const REG_KEY_BETA = '8237E44A-0054-442C-B6B6-EA0509993955';
+    private const REG_KEY_DEV = '401C381F-E0DE-4B85-8BD8-3F3F14FBDA57';
+    private const REG_KEY_CANARY = '4ea16ac7-fd5a-47c3-875b-dbf4a2008c20';
+    private const VERSION_REG_QUERY_LOCAL_MACHINE = 'reg query HKLM\Software\Google\Update\Clients\{%s} /v pv /reg:32 2> NUL';
+    private const VERSION_REG_QUERY_CURRENT_USER = 'reg query HKCU\Software\Google\Update\Clients\{%s} /v pv /reg:32 2> NUL';
+
     /** @var CommandLineEnvironment */
     private $commandLineEnvironment;
 
@@ -37,9 +45,16 @@ final class VersionResolver implements VersionResolverInterface
         }
 
         if ($operatingSystem->equals(OperatingSystem::WINDOWS())) {
-            return $this->getVersionFromCommandLine(
-                sprintf('wmic datafile where name="%s" get Version /value', $path)
-            );
+            $previousException = null;
+            foreach (self::getWindowsCommandsForVersion() as $possibleCommand) {
+                try {
+                    return $this->getVersionFromCommandLine($possibleCommand);
+                } catch (InvalidArgumentException $exception) {
+                    $previousException = $exception;
+                }
+            }
+
+            throw new InvalidArgumentException('Version could not be determined.', 0, $previousException);
         }
 
         throw NotImplemented::feature(
@@ -61,12 +76,36 @@ final class VersionResolver implements VersionResolverInterface
             $commandOutput = $this->commandLineEnvironment->getCommandLineSuccessfulOutput($command);
 
             return Version::fromString($commandOutput);
-        } catch (RuntimeException $exception) {
-            throw new RuntimeException(
+        } catch (InvalidArgumentException $exception) {
+            throw new InvalidArgumentException(
                 'Version could not be determined.',
                 0,
                 $exception
             );
         }
+    }
+
+    /**
+     * Provide potential commands to determine Chrome Version on Windows
+     *
+     * @see https://bugs.chromium.org/p/chromium/issues/attachmentText?aid=387709
+     * @see https://bugs.chromium.org/p/chromium/issues/detail?id=158372
+     *
+     * @return string[]
+     *
+     * @throws StringsException
+     */
+    private static function getWindowsCommandsForVersion(): array
+    {
+        $commands = [];
+        foreach ([self::REG_KEY_STABLE, self::REG_KEY_BETA, self::REG_KEY_DEV] as $regKey) {
+            $commands[] = sprintf(self::VERSION_REG_QUERY_LOCAL_MACHINE, $regKey);
+        }
+
+        foreach ([self::REG_KEY_STABLE, self::REG_KEY_BETA, self::REG_KEY_DEV, self::REG_KEY_CANARY] as $regKey) {
+            $commands[] = sprintf(self::VERSION_REG_QUERY_CURRENT_USER, $regKey);
+        }
+
+        return $commands;
     }
 }
