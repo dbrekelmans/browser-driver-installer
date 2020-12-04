52f84897d4caa6893297207560c7dae3a38d4469
Laurent Clouet <laurent.clouet@westwing.pl>
2020-12-04 23:08:26 +0100

    Add Firefox support

---
+++ src/Browser/Firefox/VersionResolver.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBrekelmans\BrowserDriverInstaller\Browser\Firefox;
+
+use DBrekelmans\BrowserDriverInstaller\Browser\BrowserName;
+use DBrekelmans\BrowserDriverInstaller\Browser\VersionResolver as VersionResolverInterface;
+use DBrekelmans\BrowserDriverInstaller\CommandLine\CommandLineEnvironment;
+use DBrekelmans\BrowserDriverInstaller\Exception\NotImplemented;
+use DBrekelmans\BrowserDriverInstaller\OperatingSystem\OperatingSystem;
+use DBrekelmans\BrowserDriverInstaller\Version;
+use InvalidArgumentException;
+
+use function Safe\sprintf;
+
+final class VersionResolver implements VersionResolverInterface
+{
+    /** @var CommandLineEnvironment */
+    private $commandLineEnvironment;
+
+    public function __construct(CommandLineEnvironment $commandLineEnvironment)
+    {
+        $this->commandLineEnvironment = $commandLineEnvironment;
+    }
+
+    public function from(OperatingSystem $operatingSystem, string $path): Version
+    {
+        if ($operatingSystem->equals(OperatingSystem::LINUX())) {
+            return $this->getVersionFromCommandLine(sprintf('%s --version', $path));
+        }
+
+        if ($operatingSystem->equals(OperatingSystem::MACOS())) {
+            return $this->getVersionFromCommandLine(
+                sprintf('%s/Contents/MacOS/firefox --version', $path)
+            );
+        }
+
+        if ($operatingSystem->equals(OperatingSystem::WINDOWS())) {
+            return $this->getVersionFromCommandLine(sprintf('%s --version', $path));
+        }
+
+        throw NotImplemented::feature(
+            sprintf(
+                'Resolving version on %s',
+                $operatingSystem->getValue()
+            )
+        );
+    }
+
+    public function supports(BrowserName $browserName): bool
+    {
+        return $browserName->equals(BrowserName::FIREFOX());
+    }
+
+    private function getVersionFromCommandLine(string $command): Version
+    {
+        try {
+            $commandOutput = $this->commandLineEnvironment->getCommandLineSuccessfulOutput($command);
+
+            return Version::fromString($commandOutput);
+        } catch (InvalidArgumentException $exception) {
+            throw new InvalidArgumentException(
+                'Version could not be determined.',
+                0,
+                $exception
+            );
+        }
+    }
+}
---
+++ tests/Browser/Firefox/VersionResolverTest.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBrekelmans\BrowserDriverInstaller\Tests\Browser\Firefox;
+
+use DBrekelmans\BrowserDriverInstaller\Browser\BrowserName;
+use DBrekelmans\BrowserDriverInstaller\Browser\Firefox\VersionResolver;
+use DBrekelmans\BrowserDriverInstaller\CommandLine\CommandLineEnvironment;
+use DBrekelmans\BrowserDriverInstaller\OperatingSystem\OperatingSystem;
+use DBrekelmans\BrowserDriverInstaller\Version;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class VersionResolverTest extends TestCase
+{
+    /** @var VersionResolver */
+    private $versionResolver;
+
+    /** @var MockObject&CommandLineEnvironment */
+    private $commandLineEnvMock;
+
+    public function testDoesNotSupportChrome(): void
+    {
+        self::assertFalse($this->versionResolver->supports(BrowserName::GOOGLE_CHROME()));
+    }
+
+    public function testSupportsFirefox(): void
+    {
+        self::assertTrue($this->versionResolver->supports(BrowserName::FIREFOX()));
+    }
+
+    public function testFromLinux(): void
+    {
+        $this->mockCommandLineCommandOutput('firefox --version', 'Mozilla Firefox 83.0');
+
+        self::assertEquals(
+            Version::fromString('83.0'),
+            $this->versionResolver->from(OperatingSystem::LINUX(), 'firefox')
+        );
+    }
+
+    public function testFromMac(): void
+    {
+        $this->mockCommandLineCommandOutput(
+            '/Applications/Firefox.app/Contents/MacOS/firefox --version',
+            'Mozilla Firefox 83.0'
+        );
+
+        self::assertEquals(
+            Version::fromString('83.0'),
+            $this->versionResolver->from(OperatingSystem::MACOS(), '/Applications/Firefox.app')
+        );
+    }
+
+    public function testFromWindows(): void
+    {
+        $this->mockCommandLineCommandOutput(
+            'C:\\Program Files (x86)\\Firefox\\Application\\firefox.exe --version',
+            'Mozilla Firefox 83.0'
+        );
+
+        self::assertEquals(
+            Version::fromString('83.0'),
+            $this->versionResolver->from(OperatingSystem::WINDOWS(), 'C:\\Program Files (x86)\\Firefox\\Application\\firefox.exe')
+        );
+    }
+
+    protected function setUp(): void
+    {
+        $this->commandLineEnvMock = $this->createMock(CommandLineEnvironment::class);
+        $this->versionResolver = new VersionResolver($this->commandLineEnvMock);
+    }
+
+    private function mockCommandLineCommandOutput(string $command, string $output): void
+    {
+        $this->commandLineEnvMock
+            ->method('getCommandLineSuccessfulOutput')
+            ->with($command)
+            ->willReturn($output);
+    }
+}
