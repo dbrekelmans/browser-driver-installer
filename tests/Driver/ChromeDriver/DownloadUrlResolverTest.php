66bb74c70cea962e1c7e115aa6360d20b5442b21
Kevin van Sonsbeek <kevin_787@msn.com>
2024-02-20 18:33:13 +0100

    Feature: Updated test, to remove the binary param, and test all OS version on the old and new download endpoint

--- tests/Driver/ChromeDriver/DownloadUrlResolverTest.php
+++ tests/Driver/ChromeDriver/DownloadUrlResolverTest.php
@@ -20,29 +20,47 @@ final class DownloadUrlResolverTest extends TestCase
     private DownloadUrlResolver $urlResolver;
 
     /**
-     * @return iterable<string, array{0: Driver, 1: string, 2: string}>
+     * @return iterable<string, array{0: Driver, 1: string}>
      */
     public static function byDriverDataProvider(): iterable
     {
-        yield 'legacy version' => [
+        yield 'legacy version linux' => [
             new Driver(DriverName::CHROME(), Version::fromString('88.0.4299.0'), OperatingSystem::LINUX()),
-            'some_binary',
-            'https://chromedriver.storage.googleapis.com/88.0.4299.0/some_binary.zip',
+            'https://chromedriver.storage.googleapis.com/88.0.4299.0/chromedriver_linux64.zip',
         ];
 
-        yield 'new version' => [
+        yield 'legacy version macos' => [
+            new Driver(DriverName::CHROME(), Version::fromString('88.0.4299.0'), OperatingSystem::MACOS()),
+            'https://chromedriver.storage.googleapis.com/88.0.4299.0/chromedriver_mac64.zip',
+        ];
+
+        yield 'legacy version windows' => [
+            new Driver(DriverName::CHROME(), Version::fromString('88.0.4299.0'), OperatingSystem::WINDOWS()),
+            'https://chromedriver.storage.googleapis.com/88.0.4299.0/chromedriver_win32.zip',
+        ];
+
+        yield 'new version linux' => [
             new Driver(DriverName::CHROME(), Version::fromString('115.0.5790.170'), OperatingSystem::LINUX()),
-            'linux64',
             'https://dynamic-url-2/',
+        ];
+
+        yield 'new version macos' => [
+            new Driver(DriverName::CHROME(), Version::fromString('115.0.5790.170'), OperatingSystem::MACOS()),
+            'https://dynamic-url-3/',
+        ];
+
+        yield 'new version windows' => [
+            new Driver(DriverName::CHROME(), Version::fromString('115.0.5790.170'), OperatingSystem::WINDOWS()),
+            'https://dynamic-url-1/',
         ];
     }
 
     /**
      * @dataProvider byDriverDataProvider
      */
-    public function testByDriver(Driver $driver, string $binaryName, string $expectedUrl): void
+    public function testByDriver(Driver $driver, string $expectedUrl): void
     {
-        self::assertSame($expectedUrl, $this->urlResolver->byDriver($driver, $binaryName));
+        self::assertSame($expectedUrl, $this->urlResolver->byDriver($driver));
     }
 
     protected function setUp(): void
@@ -59,6 +77,7 @@ final class DownloadUrlResolverTest extends TestCase
                                             'chromedriver' => [
                                                 ['platform' => 'win32', 'url' => 'https://dynamic-url-1/'],
                                                 ['platform' => 'linux64', 'url' => 'https://dynamic-url-2/'],
+                                                ['platform' => 'mac-x64', 'url' => 'https://dynamic-url-3/'],
                                             ],
                                         ],
                                     ],
