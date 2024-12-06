2c4c92011253f98a5c400a25f960fb1d188fbd22
Michael <m.picard@oieau.fr>
2024-12-06 14:21:18 +0100

    Fix phpcs error

--- src/Cpu/CpuArchitecture.php
+++ src/Cpu/CpuArchitecture.php
@@ -9,13 +9,13 @@ use function php_uname;
 enum CpuArchitecture: string
 {
     case AMD64 = 'amd64';
-    case ARM64  = 'arm64';
+    case ARM64 = 'arm64';
 
     public function toString(): string
     {
         return match ($this) {
             self::AMD64 => ' amd64',
-            self::ARM64  => ' arm64',
+            self::ARM64 => ' arm64',
         };
     }
 
